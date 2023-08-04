bec96a3390d11722975cbcf42c4b9b865c0f06ab
Hữu Khương Lê <ilehuukhuong@gmail.com>
2023-08-04 14:14:40 +0700

    Update LikeRepository

--- API/Data/Repository/LikesRepository.cs
+++ API/Data/Repository/LikesRepository.cs
@@ -17,11 +17,12 @@ namespace API.Data.Repository
 
         public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
         {
-             return await _context.Likes.FindAsync(sourceUserId, targetUserId);
+            return await _context.Likes.FindAsync(sourceUserId, targetUserId);
         }
 
         public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
         {
+            var sourceUser = await _context.Users.Where(x => x.Id == likesParams.UserId).Include(x => x.LikedUsers).Include(x => x.LikedByUsers).FirstOrDefaultAsync();
             var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
             var likes = _context.Likes.AsQueryable();
 
@@ -29,12 +30,36 @@ namespace API.Data.Repository
             {
                 likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
                 users = likes.Select(like => like.TargetUser);
+                foreach (var user in sourceUser.LikedByUsers)
+                {
+                    users = users.Where(x => x.Id != user.SourceUserId);
+                }
             }
 
             if (likesParams.Predicate == "likedBy")
             {
                 likes = likes.Where(like => like.TargetUserId == likesParams.UserId);
                 users = likes.Select(like => like.SourceUser);
+                foreach (var user in sourceUser.LikedUsers)
+                {
+                    users = users.Where(x => x.Id != user.TargetUserId);
+                }
+            }
+
+            if (likesParams.Predicate == "connected")
+            {
+                // Users who liked the source user
+                var likedByUsers = _context.Likes
+                    .Where(like => like.TargetUserId == likesParams.UserId)
+                    .Select(like => like.SourceUser);
+
+                // Users liked by the source user
+                var likedUsersTemp = _context.Likes
+                    .Where(like => like.SourceUserId == likesParams.UserId)
+                    .Select(like => like.TargetUser);
+
+                // Intersection of the two lists
+                users = likedUsersTemp.Intersect(likedByUsers);
             }
 
             var likedUsers = users.Select(user => new LikeDto
