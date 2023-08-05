0c343829eccedeb471b241b390ae336d1f645e8c
Hữu Khương Lê <ilehuukhuong@gmail.com>
2023-08-05 15:26:01 +0700

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -19,21 +19,22 @@ if (builder.Environment.IsDevelopment())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else
 {
-    // Use connection string provided at runtime by Server.
-    var connUrl = Environment.GetEnvironmentVariable("NEON_POSTGRESQL_CONNECTIONSTRING");
+    // // Use connection string provided at runtime by Server.
+    // var connUrl = Environment.GetEnvironmentVariable("DefaultConnection");
 
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
+    // // Parse connection URL to connection string for Npgsql
+    // connUrl = connUrl.Replace("postgres://", string.Empty);
+    // var pgUserPass = connUrl.Split("@")[0];
+    // var pgHostPortDb = connUrl.Split("@")[1];
+    // var pgHostPort = pgHostPortDb.Split("/")[0];
+    // var pgDb = pgHostPortDb.Split("/")[1];
+    // var pgUser = pgUserPass.Split(":")[0];
+    // var pgPass = pgUserPass.Split(":")[1];
+    // var pgHost = pgHostPort.Split(":")[0];
+    // var pgPort = pgHostPort.Split(":")[1];
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    // connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = Environment.GetEnvironmentVariable("DefaultConnection");
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
