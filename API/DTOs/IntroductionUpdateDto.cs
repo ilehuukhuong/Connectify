7a3b4404ee6aaf0d00cd2f2b8c77da8aff66844f
Hữu Khương Lê <ilehuukhuong@gmail.com>
2023-08-08 10:18:54 +0700

    Update Max Word Introduction

--- API/DTOs/IntroductionUpdateDto.cs
+++ API/DTOs/IntroductionUpdateDto.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.DTOs
 {
     public class IntroductionUpdateDto
     {
         [Required(ErrorMessage = "Please enter an introduction")]
+        [MaxWord(200, ErrorMessage = "The introduction must be less than 200 words.")]
         public string Introduction { get; set; }
     }
 }
---
+++ API/Extensions/MaxWordAttribute.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Extensions
+{
+    public class MaxWordAttribute : ValidationAttribute
+    {
+        private readonly int _maxWords;
+
+        public MaxWordAttribute(int maxWords)
+        {
+            _maxWords = maxWords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var wordCount = value.ToString().Split(' ').Length;
+                if (wordCount > _maxWords)
+                {
+                    return new ValidationResult($"The field {validationContext.DisplayName} must be less than {_maxWords} words.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
